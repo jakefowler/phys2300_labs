214804607e1de5d9a7a7af78228eab5aea1ad20f
jakefowler <46506569+jakefowler@users.noreply.github.com>
2019-04-01 20:54:05 -0600

    Inititial pendulum without friction

--- lab6/pendulum_template.py
+++ lab6/pendulum_template.py
@@ -1,5 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from vpython import *
 
 g = 9.81    # m/s**2
 l = 0.1     # meters
@@ -7,6 +8,8 @@ W = 0.002   # arm radius
 R = 0.01     # ball radius
 framerate = 100
 steps_per_frame = 10
+x_coordinates = []
+y_coordinates = []
 
 def f(r):
     """
@@ -28,18 +31,37 @@ def main():
     # Initial x and y
     x = l*np.sin(r[0])
     y = -l*np.cos(r[0])
+    
+    ball = sphere(pos=vector(x, y, 0), radius=R, color=color.red)
+    arm = cylinder(pos=vector(0, 0, 0), axis=vector(x, y, 0), radius=W)
+    ceiling = box(pos=vector(0, 0, 0), size=vector(0.05, 0.001, 0.05))
 
     # Loop over some time interval
     dt = 0.01
     t = 0
-    while t < 10:
-        # Use the 4'th order Runga-Kutta approximation
-#        for i in range(steps_per_frame):
-        r += h*f(r)
+    while t < 100:
+            rate(100)
+            # Use the 4'th order Runga-Kutta approximation
+            # #       for i in range(steps_per_frame):
+            r += h*f(r)
 
-        t += dt
-        # Update positions
-        x = l*np.sin(r[0])
-        y = -l*np.cos(r[0])
-        # Update the cylinder axis
-        # Update the pendulum's bob
+            t += dt
+            # Update positions
+            x = l*np.sin(r[0])
+            y = -l*np.cos(r[0])
+
+            ball.pos = vector(x, y, 0)
+            arm.axis = vector(x, y, 0)
+
+            x_coordinates.append(x)
+            y_coordinates.append(y)
+                
+            # Update the cylinder axis
+            # Update the pendulum's bob
+
+    plt.plot(x_coordinates, y_coordinates)
+    plt.show()
+
+if __name__ == "__main__":
+    main()
+   
